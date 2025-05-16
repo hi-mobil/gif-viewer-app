dabc467448b6da23f63dbee953fe901e0d10ba80
hi-mobil <hi_mobil@naver.com>
2025-05-17 00:46:58 +0900

    Update web build with correct base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2348736029"
+    serviceWorkerVersion: "1901980573"
   }
 });
